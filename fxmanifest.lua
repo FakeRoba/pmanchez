69d21e07dbf149e993202359226982e60779c2a4
Roba <127531030+FakeRoba@users.noreply.github.com>
2024-04-16 03:12:00 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 game 'gta5'
 fx_version 'cerulean'
-
 author 'Roba'
+
+client_script 'vehicle_names.lua'
 
 files {
     'vehicles.meta',
@@ -16,4 +17,3 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'DLC_TEXT_FILE' 'dlctext.meta'
-client_script 'vehicle_names.lua'
